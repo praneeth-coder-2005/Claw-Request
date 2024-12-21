09264b64da67375166d0b7d18033cc22ab6acffd
praneeth-coder-2005 <savediskofc@gmail.com>
2024-12-21 16:43:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ MONGODB_URI = "mongodb+srv://claw-earning:5213680099@cluster0.je3b1k7.mongodb.ne
 MONGODB_DATABASE_NAME = "sample_mflix"  # Add this line
 ADMIN_USER_IDS = [1894915577]  # Replace with Telegram User IDs that are admins
 TMDB_API_KEY = "bb5f40c5be4b24660cbdc20c2409835e"  
+KOYEB_APP_URL = "https://significant-aurelia-claw-filesyw-0ceb5e09.koyeb.app/"
