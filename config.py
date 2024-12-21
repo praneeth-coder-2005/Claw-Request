bb69140e4d3c14942a2bce8e82b0aa5849103e0e
praneeth-coder-2005 <savediskofc@gmail.com>
2024-12-21 12:45:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 TELEGRAM_BOT_TOKEN = "7929866615:AAGv3uzJjchHhc1ws2KoGwXHyySpegHCH-4"  # Replace with your bot token
-MONGODB_URI = " mongodb+srv://claw-earning:5213680099@cluster0.je3b1k7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  # Replace with your MongoDB URI
+MONGODB_URI = "mongodb+srv://claw-earning:5213680099@cluster0.je3b1k7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  # Replace with your MongoDB URI
 MONGODB_DATABASE_NAME = "Claw_Files"  # Add this line
 ADMIN_USER_IDS = [1894915577]  # Replace with Telegram User IDs that are admins
