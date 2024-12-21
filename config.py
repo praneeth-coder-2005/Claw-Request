dbf7d8effa31746ffde7f1684937dd0772fe61bb
praneeth-coder-2005 <savediskofc@gmail.com>
2024-12-21 12:17:46 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+TELEGRAM_BOT_TOKEN = "7929866615:AAGv3uzJjchHhc1ws2KoGwXHyySpegHCH-4"  # Replace with your bot token
+MONGODB_URI = "mongodb+srv://claw-earning:5213680099@cluster0.je3b1k7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  # Replace with your MongoDB URI
+ADMIN_USER_IDS = [1894915577]  # Replace with Telegram User IDs that are admins
