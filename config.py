8438b05ee50335f09da8edf3e558809589d4da6f
praneeth-coder-2005 <savediskofc@gmail.com>
2025-06-23 13:57:55 +0530

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-TELEGRAM_BOT_TOKEN = "7929866615:AAGv3uzJjchHhc1ws2KoGwXHyySpegHCH-4"  # Replace with your bot token
-MONGODB_URI = "mongodb+srv://claw-earning:5213680099@cluster0.je3b1k7.mongodb.net/sample_mflix?retryWrites=true&w=majority&appName=Cluster0"  # Replace with your MongoDB URI
-MONGODB_DATABASE_NAME = "sample_mflix"  # Add this line
-ADMIN_USER_IDS = [1894915577]  # Replace with Telegram User IDs that are admins
-TMDB_API_KEY = "bb5f40c5be4b24660cbdc20c2409835e"  
-KOYEB_APP_URL = "https://significant-aurelia-claw-filesyw-0ceb5e09.koyeb.app/"
